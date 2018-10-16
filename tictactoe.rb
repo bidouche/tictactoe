f64cd74741b73a78e4b4a42e29ef76227ea49752
bidouche <alexetiennelev@gmail.com>
2018-10-17 01:55:05 +0200

    correction

--- tictactoe.rb
+++ tictactoe.rb
@@ -11,7 +11,7 @@ class Board
   def initialize
     @board = []
     i = 0
-    #je crée mes instance de BordCase
+    #je crée mes instances de BordCase
     while (i < 10)
       @board << @board[i] = BoardCase.new(nil, i)
       i +=1
@@ -19,7 +19,7 @@ class Board
   end
 
   def victory?
-    #dans win_comb je rentre toute les possibilité de gagné
+    #dans win_comb je rentre toutes les possibilités de gagner
     win_comb = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
     if (win_comb.include?(@player1_plays.sort))
       "Victoire de #{@player1}"
@@ -48,11 +48,12 @@ class Board
   puts "Player 2 : name"
   print ">"
   @player2 = gets.chomp
-  #puts "Dans ce jeu vous choisirez vos cases avec des chiffres de 1 à 9, 1 étant la case en haut a gauche et 9 la case en bas a droite"
+  
   if (signe_player1 == "X") then signe_player2 = "O"
   end
   if (signe_player1 == "O") then signe_player2 = "X"
   end
+  
   i = 0
   while (victory? == nil && i < 9)
   plate = "#{board[1].value} | #{board[2].value} | #{board[3].value}","----------","#{board[4].value} | #{board[5].value} | #{board[6].value}","----------","#{board[7].value} | #{board[8].value} | #{board[9].value}" 
@@ -61,7 +62,7 @@ class Board
       puts "Your turn #{@player1}"
       print ">"
       input = gets.chomp.to_i
-      @player1_plays << input # je pousse les réponse du joueur dans un array pour le comparer a mon array de la méthode victory?
+      @player1_plays << input # je pousse les réponses du joueur dans un array pour le comparer a mon array de la méthode victory?
       @board[input].value = signe_player1 # je rentre la nouvelle valeur dans mon plate
     else
       puts "Your turn #{@player2}"
